0ed67f2a09b4877e633853dc2aad28d0703415ff
Nate deVelder <ndevelde@umass.edu>
2016-03-22 11:44:59 -0500

    First Commit

---
+++ lnInclude/pimpleForceFoam.C
@@ -0,0 +1,1 @@
+../pimpleForceFoam.C
---
+++ pimpleForceFoam.C
@@ -0,0 +1,113 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | foam-extend: Open Source CFD
+   \\    /   O peration     |
+    \\  /    A nd           | For copyright notice see file Copyright
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of foam-extend.
+
+    foam-extend is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by the
+    Free Software Foundation, either version 3 of the License, or (at your
+    option) any later version.
+
+    foam-extend is distributed in the hope that it will be useful, but
+    WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with foam-extend.  If not, see <http://www.gnu.org/licenses/>.
+
+Application
+    pimpleFoam
+
+Description
+    Large time-step transient solver for incompressible, flow using the PIMPLE
+    (merged PISO-SIMPLE) algorithm.
+
+    Turbulence modelling is generic, i.e. laminar, RAS or LES may be selected.
+
+\*---------------------------------------------------------------------------*/
+
+#include "fvCFD.H"
+#include "singlePhaseTransportModel.H"
+#include "turbulenceModel.H"
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+int main(int argc, char *argv[])
+{
+#   include "setRootCase.H"
+#   include "createTime.H"
+#   include "createMesh.H"
+#   include "createFields.H"
+#   include "initContinuityErrs.H"
+
+IOdictionary transportProperties
+(
+     IOobject
+     (
+          "transportProperties",
+          runTime.constant(),
+          mesh,
+          IOobject::MUST_READ,
+          IOobject::NO_WRITE
+     )
+);
+
+const word solveTurbOnly(transportProperties.lookup("solveTurbOnly"));
+
+    Info<< "\nStarting time loop\n" << endl;
+
+    while (runTime.run())
+    {
+#       include "readTimeControls.H"
+#       include "readPIMPLEControls.H"
+#       include "CourantNo.H"
+#       include "setDeltaT.H"
+
+        runTime++;
+
+        Info<< "Time = " << runTime.timeName() << nl << endl;
+
+        // --- Pressure-velocity PIMPLE corrector loop
+        for (int oCorr = 0; oCorr < nOuterCorr; oCorr++)
+        {
+
+           if (solveTurbOnly != "true")
+            {
+
+            if (nOuterCorr != 1)
+            {
+                p.storePrevIter();
+            }
+
+#               include "UEqn.H"
+
+            // --- PISO loop
+            for (int corr = 0; corr < nCorr; corr++)
+            {
+#               include "pEqn.H"
+            }
+
+         }
+            turbulence->correct();
+        }
+
+        runTime.write();
+
+        Info<< "ExecutionTime = " << runTime.elapsedCpuTime() << " s"
+            << "  ClockTime = " << runTime.elapsedClockTime() << " s"
+            << nl << endl;
+    }
+
+    Info<< "End\n" << endl;
+
+    return 0;
+}
+
+
+// ************************************************************************* //
